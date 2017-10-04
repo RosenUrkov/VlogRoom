e4553f508cc6f1c35b8b661e50341833fbd5e895
Rosen Urkov <rosen.urkov@gmail.com>
2017-10-04 20:30:52 +0300

    Adding video view and model

---
+++ VlogRoom/VlogRoom.Web/Models/VideoDataViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VlogRoom.Web.Models
+{
+    public class VideoDataViewModel
+    {
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public bool IsValid { get; set; }
+    }
+}

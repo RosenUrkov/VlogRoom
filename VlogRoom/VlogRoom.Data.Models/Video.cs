dcbf0deb123b4ae2a11abeaf234886f7cc8a1ed3
Rosen Urkov <rosen.urkov@gmail.com>
2017-10-07 20:23:59 +0300

    Added video model

---
+++ VlogRoom/VlogRoom.Data.Models/Video.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VlogRoom.Data.Models.Abstractions;
+using VlogRoom.Data.Models.Contracts;
+
+namespace VlogRoom.Data.Models
+{
+    public class Video : BaseModel, IAuditable, IDeletable
+    {
+
+    }
+}

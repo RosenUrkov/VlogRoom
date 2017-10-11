cb5d40154f832b0ee0b7d6120ab0e41f230ab6a7
Rosen Urkov <rosen.urkov@gmail.com>
2017-10-11 22:57:40 +0300

    Added video template

--- VlogRoom/VlogRoom.Web/Scripts/Custom/custom.js
+++ VlogRoom/VlogRoom.Web/Scripts/Custom/custom.js
@@ -14,3 +14,17 @@ $(function () {
         }
     });
 });
+
+$(document).ready(function () {
+	size_li = $("#myList li").length;
+	x=1;
+	$('#myList li:lt('+x+')').show();
+	$('#loadMore').click(function () {
+		x= (x+1 <= size_li) ? x+1 : size_li;
+		$('#myList li:lt('+x+')').show();
+	});
+	$('#showLess').click(function () {
+		x=(x-1<0) ? 1 : x-1;
+		$('#myList li').not(':lt('+x+')').hide();
+	});
+});

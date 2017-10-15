b5dc0ab5782cd323750202530938597970aed024
Rosen Urkov <rosen.urkov@gmail.com>
2017-10-15 21:29:50 +0300

    Added sample tests to test jenkins

--- VlogRoom/VlogRoom.Services.Data.Tests/UserDataServiceTests/GetUserByIdShould.cs
+++ VlogRoom/VlogRoom.Services.Data.Tests/UserDataServiceTests/GetUserByIdShould.cs
@@ -1,9 +1,12 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VlogRoom.Data.Models;
+using VlogRoom.Data.Repository;
 
 namespace VlogRoom.Services.Data.Tests.UserDataServiceTests
 {
@@ -13,7 +16,68 @@ namespace VlogRoom.Services.Data.Tests.UserDataServiceTests
         [Test]
         public void CallUsersRepoAllMethodOnce()
         {
-            Assert.AreEqual(1, 1);
+            // arrange
+            var userId = "id";
+
+            var user = new User();
+            var usersCollection = new List<User>() { user }.AsQueryable();
+
+            var repoMock = new Mock<IEfRepository<User>>();
+            repoMock.Setup(x => x.All).Returns(usersCollection);
+
+            var userDataService = new UserDataService(repoMock.Object);
+
+            // act
+            userDataService.GetUserById(userId);
+
+            // assert
+            repoMock.Verify(x => x.All, Times.Once());
+        }
+
+        [Test]
+        public void ReturnTheUserWithTheSearchedId()
+        {
+            // arrange
+            var userId = "id";
+
+            var user = new User();
+            user.Id = userId;
+
+            var usersCollection = new List<User>() { user }.AsQueryable();
+
+            var repoMock = new Mock<IEfRepository<User>>();
+            repoMock.Setup(x => x.All).Returns(usersCollection);
+
+            var userDataService = new UserDataService(repoMock.Object);
+
+            // act
+            var foundUser = userDataService.GetUserById(userId);
+
+            // assert
+            Assert.AreEqual(user, foundUser);
+        }
+
+        [Test]
+        public void ReturnNullIfThereIsNoSuchUser()
+        {
+            // arrange
+            var userId = "id";
+
+            var user = new User();
+            user.Id = userId;
+
+            var usersCollection = new List<User>() { user }.AsQueryable();
+
+            var repoMock = new Mock<IEfRepository<User>>();
+            repoMock.Setup(x => x.All).Returns(usersCollection);
+
+            var userDataService = new UserDataService(repoMock.Object);
+
+            // act
+            var foundUser = userDataService.GetUserById("other id");
+
+            // assert
+            Assert.IsNull(foundUser);
         }
     }
 }
